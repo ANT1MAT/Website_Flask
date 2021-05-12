1f237f8a3793ce78f9d4500ea826d3bb7546d670
Sergey Telepnev <sergej.t@ati.su>
2021-05-12 10:51:39 +0300

    add comments

--- myapp/app.py
+++ myapp/app.py
@@ -1,19 +1,21 @@
+# Порядок
 import os.path
 from flask import Flask
-from myapp.products import products_blueprints
 from database import db
+from myapp.products import products_blueprints
 from myapp.mainpage import mainpage_blueprints
+
 
 def create_app():
     # Конфигурация
     app = Flask(__name__)
     app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database/database.db'
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-    #app.config['UPLOAD_FOLDER'] = 'static/uploads'
     app.config['SECRET_KEY'] = 'FgrtGFr43Etr'
-    ALLOWED_EXTENSIONS = {'txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'}
     db.init_app(app)
+    # db exists
     if os.path.exists('myapp/database/database.db'):
+        # logging ?
         print('Data Base already create')
     else:
         db.create_all(app=app)
@@ -21,7 +23,6 @@ def create_app():
     products_blueprints(app)
     mainpage_blueprints(app)
     return app
-
 
 
 if __name__ == "__main__":
--- myapp/products/products.py
+++ myapp/products/products.py
@@ -7,29 +7,39 @@ from werkzeug.utils import secure_filename
 import datetime
 
 
-products_blueprint = Blueprint('prod', __name__,
-                           template_folder='templates/html',
-                           static_folder='static',
-                           url_prefix='/product')
+products_blueprint = Blueprint(
+    'prod', __name__,
+    template_folder='templates/html',
+    static_folder='static',
+    url_prefix='/product'
+)
 
 
-@products_blueprint.route('/prod_add', methods=['post', 'get'])
+@products_blueprint.route('/prod_add', methods=['get'])
 def add_prod_page():
-    if request.method == 'POST':
-        logging.info('Add data in DB')
-        file = request.files['prod_img']
-        img_name = secure_filename(str(datetime.datetime.now()))
-        try:
-            add = Products(name=request.form['name'], price=request.form['price'], img=img_name)
-            db.session.add(add)
-            file.save(os.path.join('myapp/products/static/uploads', img_name))
-            db.session.commit()
-        except Exception as e:
-            db.session.rollback()
-            logging.error(f'Error, {e}')
     return render_template('products_add.html')
 
 
+@products_blueprint.route('/prod_add', methods=['post'])
+def add_prod_page():
+    logging.info('Add data in DB')
+    file = request.files['prod_img']
+    img_name = secure_filename(str(datetime.datetime.now()))
+    try:
+        add = Products(name=request.form['name'], price=request.form['price'],
+                       img=img_name)
+        db.session.add(add)
+        file.save(os.path.join('myapp/products/static/uploads', img_name))
+        db.session.commit()
+        # 200 / 201
+    # Нормально ловить и отдавать ошибки
+    except Exception as e:
+        db.session.rollback()
+        logging.error(f'Error, {e}')
+        # 400
+
+
+# Разделить, 200
 @products_blueprint.route('/prod_menu/', methods=['post', 'get'])
 def prod_menu():
     menu = Products.query.all()
@@ -39,6 +49,7 @@ def prod_menu():
     return render_template('prod_menu.html', menu=menu)
 
 
+# Также
 @products_blueprint.route('/id<prod_id>', methods=['post', 'get'])
 def prod_add_in_cart(prod_id):
     product = Products.query.get(prod_id)
@@ -53,6 +64,7 @@ def prod_add_in_cart(prod_id):
     return render_template('add_prod_in_cart.html', product=product)
 
 
+# Ловить ошибку и отдавать либо пустой шаблон либо 400
 @products_blueprint.route('/cart')
 def show_cart():
     product = Products.query.all()
